eecadd98b21e4c0a4d29dab3e5f2e9cf8d633a5c
MikSDigital <mixa.ru.ru@gmail.com>
2018-04-07 16:57:13 +0300

    Commit

--- src/Utils/Slugger.php
+++ src/Utils/Slugger.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: m
- * Date: 7.04.18
- * Time: 16:38
- */
 
 namespace App\Utils;
 
 
 class Slugger
 {
-
+    public static function slugify(string $string): string
+    {
+        return preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
+    }
 }
