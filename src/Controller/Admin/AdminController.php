0ab8a1cc08678f44950d22031c86ae59940987f5
MikSDigital <mixa.ru.ru@gmail.com>
2018-04-07 18:30:19 +0300

    CSS folder

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\PostRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/admin", name="admin_area")
@@ -16,10 +18,13 @@ class AdminController extends Controller
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(PostRepository $posts): Response
     {
+            $allPosts = $posts->findAll();
+
         return $this->render('admin/index.html.twig', [
-            'email' => $this->getUser() ? $this->getUser()->getUsername() : ''
+            'email' => $this->getUser() ? $this->getUser()->getUsername() : '',
+            'posts' => $allPosts
         ]);
     }
 }
